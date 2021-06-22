9ecf01ff42ea4530ecfdd1415b27be29e2c0d9d3
AAAR-Salmon <tenzikujohn@gmail.com>
2021-06-23 01:28:30 +0900

    Feature: /unkoで森山直太朗のうんこを流せるようにした

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const musics = require('./musics.json');
 
 client.once('ready', () => {
 	console.log('起動したで');
 	client.user.setActivity('うんち', { type: 'LISTENING' });
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	console.log(message.content);
 	if (message.content.includes('うんち')) {
 		message.react('839894283062607912');
@@ -15,6 +16,23 @@ client.on('message', message => {
 	if (message.content.includes('839894283062607912')) {
 		message.react('839894331711815730');
 	}
+
+	musics.forEach(async v => {
+		if (message.content.startsWith(v.command)) {
+			if (message.member.voice.channel) {
+				const connection = await message.member.voice.channel.join();
+				const dispatcher = connection.play(v.resource);
+
+				dispatcher.on('start', () => {
+					message.channel.send(`playing ${v.name}`)
+				});
+
+				dispatcher.on('finish', () => {
+					connection.disconnect();
+				});
+			}
+		}
+	});
 });
 
 client.login(Config.token);
